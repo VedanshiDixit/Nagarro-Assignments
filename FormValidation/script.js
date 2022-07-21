566e655167aa0c733e53c4edc86e82b64ac48d56
Vedanshi <vedu1193@gmail.com>
2022-07-21 19:57:45 +0530

    Form submission on wrong entry bug fixed in form validation

--- FormValidation/script.js
+++ FormValidation/script.js
@@ -94,7 +94,8 @@ $(document).ready(function (){
   }
 
   $('#btn').click(function(){
-    validation();
+    if(validation()==true) return true;
+    else return false;
   });
 
   function validation(){
