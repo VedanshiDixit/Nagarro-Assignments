78b5952a90ef85b80090ca811e7ba1bf046c6af5
Vedanshi <vedu1193@gmail.com>
2022-08-09 09:14:24 +0530

    + Rest + Rest-node + tweet-rest

---
+++ tweet-rest/routes/tweets.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../config/dbconfig");
+const orderBy = require("lodash/orderBy");
+
+router.get('/', function(req, res, next) {
+    res.send('tweets route accessed');
+  });
+ 
+  router.post("/add", function (request, response) {
+    console.log("data received from client", request.body);
+    const options = {
+        // schema is not passed here since it has been passed while creating client
+        table: "tweets",
+        records: [request.body],
+      };
+      db.insert(options, (err, res) => {
+        if (err) {
+          console.log("error in insert operation", err);
+          response.status(500).send({ error: err });
+        } else {
+          console.log(res);
+          response.send({ result: "Tweet added successfully" });
+        }
+      });
+    });
+    
+
+  /**
+ * Route for fetching all tweets from database
+ */
+router.get("/getAll", function (request, response) {
+    const options = {
+      // schema is not passed here since it has been passed while creating client
+      table: "tweets",
+      searchAttribute: "userHandle",
+      searchValue: "*",
+      attributes: ["*"],
+    };
+db.searchByValue(options, (err, res) => {
+    if (err) {
+      console.log("error occured while fetching data", err);
+      response.status(500).send({ Error: err });
+    } else {
+      console.log("result", res);
+      const tweetsData = res.data;
+      const sortedTweets = orderBy(tweetsData, ["__createdtime__"], ["desc"]);
+      response.send({ results: sortedTweets });
+    }
+  });
+});
+
+  
+
+  module.exports = router;
+  
